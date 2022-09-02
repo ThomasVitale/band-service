3db0d2c1185be9b4dcf70182408b37d186762f31
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2022-09-02 09:40:28 +0200

    Add buildpacks

--- src/main/java/com/thomasvitale/bandservice/BandServiceApplication.java
+++ src/main/java/com/thomasvitale/bandservice/BandServiceApplication.java
@@ -1,7 +1,13 @@
 package com.thomasvitale.bandservice;
 
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.RouterFunctions;
+import org.springframework.web.servlet.function.ServerResponse;
 
 @SpringBootApplication
 public class BandServiceApplication {
@@ -10,4 +16,13 @@ public class BandServiceApplication {
 		SpringApplication.run(BandServiceApplication.class, args);
 	}
 
+	@Bean
+	RouterFunction<ServerResponse> routerFunction() {
+		return RouterFunctions.route()
+			.GET("/", request -> ServerResponse.ok().body("Welcome to the Band Service"))
+			.GET("/instruments", request -> ServerResponse.ok().body(List.of(
+				"piano", "guitar", "drums"
+			)))
+			.build();
+	}
 }
