ea5bc752c65f17e21209802ddc7724d2430faded
Thomas Vitale <ThomasVitale@users.noreply.github.com>
2022-09-02 09:26:01 +0200

    Initial commit

---
+++ src/main/java/com/thomasvitale/bandservice/BandServiceApplication.java
@@ -0,0 +1,13 @@
+package com.thomasvitale.bandservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BandServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BandServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thomasvitale/bandservice/BandServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thomasvitale.bandservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BandServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
